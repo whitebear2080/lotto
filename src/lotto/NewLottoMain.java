93eeed53f745590c48a8b1a41d94d6b9542b8aca
whitebear2080 <whitebear2080@naver.com>
2025-01-19 22:57:34 +0900

    test

--- src/lotto/NewLottoMain.java
+++ src/lotto/NewLottoMain.java
@@ -5,7 +5,9 @@ import java.util.Random;
 
 public class NewLottoMain {
     public static void main(String[] args) {
+
         Random random = new Random();
+
         for (int i = 1; i < 6; i++) {
             List<Integer> list = random.ints(1, 46)
                     .distinct()
